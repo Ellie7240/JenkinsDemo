e42ee77dea5495720ec9fcf8e54ef0cb2a2e83cf
Ellie <sqiorrel7240@yahoo.com.tw>
2021-04-11 22:41:47 +0800

    commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("========================");
-		System.out.println("Hello World,"+args[0]);
+		System.out.println("Hello World,"+argss[0]);
 		System.out.println("========================");
 	}
 }
